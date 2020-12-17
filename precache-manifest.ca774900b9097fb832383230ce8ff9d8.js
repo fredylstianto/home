9f690e4eef76230cedbd698769603025d1d07111
fredylstianto <fr.dragon88@gmail.com>
2020-12-17 19:46:40 +0700

    Updates

---
+++ precache-manifest.ca774900b9097fb832383230ce8ff9d8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74d4812cd10591897d6bd6db0de3141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a4ccff1be09e2fdbd2f",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "f08f5c2faeba83ba75d8",
+    "url": "/static/js/2.3b189aff.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3b189aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a4ccff1be09e2fdbd2f",
+    "url": "/static/js/main.7612201f.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "5351f4f8c8f46314f72f4a9de7b3394c",
+    "url": "/static/media/hashirshoaeb.5351f4f8.png"
+  },
+  {
+    "revision": "d76a3131143f55905ca80f31b7319631",
+    "url": "/static/media/resume.d76a3131.pdf"
+  }
+]);
